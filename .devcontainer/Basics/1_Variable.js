b864f0ffde29b65889ae987643c4d8cffbc9f76b
Shweta Choudhary <140094496+Shweta1015@users.noreply.github.com>
2024-09-11 06:11:19 +0000

    main

---
+++ .devcontainer/Basics/1_Variable.js
@@ -0,0 +1,13 @@
+// There are three types of variables ie. var, let  const
+
+/*1. const: this variable cannot be modified if once initialized.
+eg const a = 10;
+a = 20 (wrong)
+ */
+
+/*2. var: It was the first variable and only used in old programs . 
+It is globbally accessible variable. 
+eg. var a = "Radhe Krishna"*/
+
+/*3. let: It is most probably in use and the value can be modified, it's scope is block based 
+ie.(inside {})*/
---
+++ .devcontainer/Basics/2_DataTypeConversion.js
@@ -0,0 +1,25 @@
+let score = 33;
+console.log(typeof score)  
+// OR console.log(typeof (score))
+// output: number
+
+//Now we set the value of score in string and then pass the value to another variable by converting in Number type
+score = "33abc"
+let valueInNuber = Number(score);
+console.log(typeof valueInNuber);
+console.log(valueInNuber);
+/* output: Number(type)
+NaN(Not a number)  because abc cannot be converted into Number */
+
+//now we set score as true and then covert ot Number
+score = true
+valueInNuber = Number(score);
+console.log(valueInNuber);
+//output: 1 (becoz true = 1) if false then 0.
+
+//now we will covert a Number/String into Boolean
+let check = 1;
+let checkBoolean = Boolean(check);
+console.log(checkBoolean);
+/*output: true(becoz of 1)
+if 0 => false, if ""(empty string) => false, if"ShivPArvati" => true */
