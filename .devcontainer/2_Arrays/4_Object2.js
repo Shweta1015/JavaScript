4bc9e48eef778f7a1e8414a2785790a277874a61
Shweta Choudhary <140094496+Shweta1015@users.noreply.github.com>
2024-09-20 11:38:04 +0000

    main

--- .devcontainer/2_Arrays/4_Object2.js
+++ .devcontainer/2_Arrays/4_Object2.js
@@ -6,16 +6,23 @@ user.name = "tony";
 user.isLoggedIn = false;
 //console.log(user);
 
-const regularUser = {
-    email: "harry@gmail.com",
-    fullname: {
-        userfullName: {
-            firstName: "Harry",
-            lastName: "Sharma"
-        }
-    } 
+/* Objects are mutable: if i create an object and then gives the same reference to other object, if we make 
+changes in the second object the 1st object will be modified automatically as they share same address.*/
+const person = {
+    name: "Shweta",
+    age: 21,
+    language: "javascript"
 }
-//console.log(regularUser.fullname.userfullName.firstName);
+
+const x = person;
+x.age = 20;
+//console.log(person);
+
+//the delete keyword delete the property of object
+delete person.language; //or delete person["language"];
+console.log(person);
+
+
 
 //ways to concarinate objects
 const obj1 = {1: "a", 2: "b"};
@@ -42,8 +49,20 @@ console.log(users[1].email);
 console.log(Object.keys(regularUser));
 //console.log(Object.values(regularUser));
 //console.log(Object.entries(regularUser));       //convert each key-value pair in seperate array
-console.log(regularUser.hasOwnProperty('fullname'));    
+console.log(regularUser.hasOwnProperty('fullname')); 
 
+
+//Nested Objects:
+const regularUser = {
+    email: "harry@gmail.com",
+    fullname: {
+        userfullName: {
+            firstName: "Harry",
+            lastName: "Sharma"
+        }
+    } 
+}
+//console.log(regularUser.fullname.userfullName.firstName); or console.log(regularUser["fullname"]["firstName"]);
 
 
 
