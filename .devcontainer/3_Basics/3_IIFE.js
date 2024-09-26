2021b19498108fb70807f563c6d86e6ff3528a7a
Shweta Choudhary <140094496+Shweta1015@users.noreply.github.com>
2024-09-26 15:06:12 +0000

    main

--- .devcontainer/3_Basics/3_IIFE.js
+++ .devcontainer/3_Basics/3_IIFE.js
@@ -1,1 +1,17 @@
 //Immediately Invoked Function Expressions(IIFE)
+
+//Basic
+function code(){
+    console.log(`DB CONNECTED`);
+}
+code();  //function calling
+
+//immediate invoke: first parenthesis ie.() for function and second () is for calling.
+(function coding(){   //named IIFE ie name is coding 
+    console.log(`You can access database.`);   
+})();
+
+//arrow function
+((name) => {
+    console.log(`Connection using Arrow function for ${name}`);  
+})('Shweta');
